37f4c45404765a34240ceb0dba3195ebc599d328
Deepak <deepaknayak42@gmail.com>
2020-04-15 15:01:38 +0530

    added if condition tochk the data

--- src/main/java/com/deepaktechie/app/git_demo/App.java
+++ src/main/java/com/deepaktechie/app/git_demo/App.java
@@ -4,10 +4,18 @@ package com.deepaktechie.app.git_demo;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public void addData(String data) {
+		System.out.println("data ," + data);
+		if(data.equals("DeepakTechie")) {
+			System.out.println("welcome");
+			
+		}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
